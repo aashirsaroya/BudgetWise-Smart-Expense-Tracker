d948196042c4b7fc2a4d791be167be571658a43b
Aashir <aashirsaroya@gmail.com>
2020-04-20 15:55:15 +0530

    Added date format package

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
@@ -47,10 +48,11 @@ class MyHomePage extends StatelessWidget {
                       width: 2,
                   )
                   ),
-                  child: Text(tx.amount.toString(),
+                  child: Text(
+                    "\u20B9" + tx.amount.toString(),
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 20 ,
+                      fontSize: 18 ,
                       color: Colors.purple ,
                     ),
                   ),
@@ -61,12 +63,12 @@ class MyHomePage extends StatelessWidget {
                   children: <Widget>[
                     Text(tx.title,
                       style: TextStyle(
-                          fontSize: 18,
+                          fontSize: 17,
                           fontWeight: FontWeight.bold
                       ) ,
                     ),
                     Text(
-                      tx.date.toString(),
+                      DateFormat.yMMMd().format(tx.date),
                       style: TextStyle(
                         color: Colors.grey,
                       ),
