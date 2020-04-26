71983610e684af0a1285514b055a27fe04772818
Aashir <aashirsaroya@gmail.com>
2020-04-26 13:47:13 +0530

    Added the chart and its functionality

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
 import './models/transaction.dart';
+import './widgets/chart.dart';
 
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
@@ -41,6 +42,12 @@ class _MyHomePageState extends State<MyHomePage> {
     //Transaction(id: 't1',title: 'New Shoes',amount:3000.0,date: DateTime.now() ),
     //Transaction(id: 't2',title: 'Groceries',amount: 220.50,date: DateTime.now())
   ];
+  List<Transaction> get _recentTransactions
+  {
+       return _userTransaction.where((tx){
+         return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+  }).toList();
+  }
   void _addNewTransaction(String txTitle,double txAmount) {
     final newTx = Transaction(
         title: txTitle,
@@ -82,11 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
           //mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Card(
-              color: Colors.blue,
-              child: Container(width: double.infinity,child: Text('Chart!!')),
-              elevation: 5,
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransaction),
           ],
         ),
